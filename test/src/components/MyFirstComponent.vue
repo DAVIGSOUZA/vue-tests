11fda6fe7a0212a284c1735e50a112f71be54f65
Davi Garcia <davigs@hotmail.com>
2021-02-05 21:29:33 -0300

    criando primeiro componente

---
+++ test/src/components/MyFirstComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class=sDiv>
+        <p>Meu primeiro componente</p>
+        <p class=toDo> Futuramente quero que ele tenha um input que mude a mensagem do componente hello world</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MyFirstComponent',
+}
+</script>
+
+<style scoped>
+.sDiv {
+    margin: 0;
+    padding: 0;
+    border: 1px solid black;
+}
+.toDo {
+    background-color: lightcoral;
+    padding: 5px;
+    /* color: whitesmoke; */
+    font-weight: bold;
+}
+
+</style>
