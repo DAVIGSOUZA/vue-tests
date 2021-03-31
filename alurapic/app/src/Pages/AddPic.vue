7de3bcdeac04fb3de9ebb4d42af781c4c900377b
Davi Garcia <davigs@hotmail.com>
2021-03-31 15:56:29 -0300

    add picture form

--- alurapic/app/src/Pages/AddPic.vue
+++ alurapic/app/src/Pages/AddPic.vue
@@ -2,15 +2,31 @@
     <div>
         <h1 class="text-center">Adicionar Fotos</h1>
         <h2 class="text-center">Cadastro</h2>
-        <form action="">
+        <form @submit.prevent="addPicture()">
             <div class="control text-center">
                 <label for="title">Titulo</label>
-                <input id="title">
+                <input 
+                    id="title" 
+                    autocomplete="off"
+                    @input="picture.title = $event.target.value"
+                >
             </div>
             <div class="control text-center">
                 <label for="url">URL</label>
-                <input id="url">
+                <input 
+                    id="url" 
+                    autocomplete="off"
+                    @input="picture.url = $event.target.value"
+                >
                 <ResponsiveImg/>
+            </div>
+            <div class="control text-center">
+                <label for="description">Descrição</label>
+                <textarea 
+                    id="description" 
+                    autocomplete="off"
+                    @input="picture.description = $event.target.value"
+                ></textarea>
             </div>
             <div class="text-center">
                 <Button name='Adicionar' type="submit" btnStyle="primary"/>
@@ -18,7 +34,6 @@
                     <Button name='Voltar' type="button"/>
                 </router-link>
             </div>
-
         </form>
     </div>
 </template>
@@ -27,10 +42,24 @@
 import ResponsiveImg from '../components/ResponsiveImg.vue'
 import Button from "../components/Button";
 export default {
-  components: { 
-    ResponsiveImg,
-    Button
+    components: { 
+        ResponsiveImg,
+        Button
     },
+    data () {
+        return {
+            picture: {
+                title: "",
+                url: "",
+                description: ""
+            }
+        }
+    },
+    methods: {
+        addPicture () {
+            console.log(this.picture)
+        }
+    }
 
 }
 </script>
